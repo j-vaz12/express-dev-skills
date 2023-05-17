35367ad07b5806c0c69981f301dc80e6b706510f
j-vaz12 <jasonvasquezx@gmail.com>
2023-05-16 22:18:42 -0700

    part1 done :)

--- models/skill.js
+++ models/skill.js
@@ -6,8 +6,14 @@ const skills = [
 
 module.exports = {
     getAll,
+    getOne
 };
 
 function getAll() {
     return skills;
+}
+
+function getOne(id) {
+    id = parseInt(id);
+    return skills.find(skill => skill.id === id); 
 }
